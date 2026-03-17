0afde3fed598a44b2b59d8f5ce100a312f302a93
xavier.tang <xavier.tang@cerence.com>
2026-03-17 11:29:14 +0800

    Finish chapter 2

--- source/chapter2/chapter2.py
+++ source/chapter2/chapter2.py
@@ -9,3 +9,29 @@ def AND_gate(x1,x2):
     if np.sum(w * x) + theta > 0:
         res = True
     print( str(x1)+" AND "+ str(x2) +" is "+ str(res))
+    return res
+
+def NAND_gate(x1,x2):
+    x=np.array([x1,x2])
+    w=np.array([-0.5,-0.5])
+    theta=0.7
+    res = False
+    if np.sum(w * x) + theta > 0:
+        res = True
+    print( str(x1)+" AND "+ str(x2) +" is "+ str(res))
+    return res
+
+def OR_gate(x1,x2):
+    x=np.array([x1,x2])
+    w=np.array([0.5,0.5])
+    theta=-0.2
+    res = False
+    if np.sum(w * x) + theta > 0:
+        res = True
+    print( str(x1)+" AND "+ str(x2) +" is "+ str(res))
+    return res
+
+def XOR_gate(x1,x2):
+    res = AND_gate(OR_gate(x1,x2),NAND_gate(x1,x2))
+    print( str(x1)+" AND "+ str(x2) +" is "+ str(res))
+    return res
--- source/main.py
+++ source/main.py
@@ -5,8 +5,12 @@ def main():
     #cp1.np_test()
     #cp1.plt_test()
     #cp1.imread_test()
-    cp2.AND_gate(0.7,0.8)
-    input("Enter to end")
+
+    #cp2.AND_gate(0,1)
+    #cp2.NAND_gate(1,1)
+    #cp2.OR_gate(1,1)
+    cp2.XOR_gate(0,0)
+    #input("Enter to end")
 
 if __name__ == "__main__":
     main()
