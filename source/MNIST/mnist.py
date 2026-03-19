4c17984e59dab7932ccc0b70b2b650663b7c7021
xavier.tang <xavier.tang@cerence.com>
2026-03-19 16:57:27 +0800

    fix mnist.py

--- source/MNIST/mnist.py
+++ source/MNIST/mnist.py
@@ -87,21 +87,3 @@ class mnist():
     def img_show(self,img):
         pil_img=Image.fromarray(np.uint8(img))
         pil_img.show()
-
-    def init_network():
-        with open("sample_weight.pkl", 'rb') as f:
-            network = pickle.load(f)
-        return network
-    
-    def predict(network, x):
-        W1, W2, W3 = network['W1'], network['W2'], network['W3']
-        b1, b2, b3 = network['b1'], network['b2'], network['b3']
-
-        a1 = np.dot(x, W1) + b1
-        z1 = sigmoid(a1)
-        a2 = np.dot(z1, W2) + b2
-        z2 = sigmoid(a2)
-        a3 = np.dot(z2, W3) + b3
-        y = softmax(a3)
-
-        return y
